47b397cfc10ad242d336b838bdc8d3a123ef875a
Maxime Journaux <journaux.maxime@gmail.com>
2013-01-07 17:36:57 +0100

    night commit Point2

--- src/lp/iem/gk/Point2.java
+++ src/lp/iem/gk/Point2.java
@@ -21,13 +21,84 @@ public class Point2 {
 	public void setX(float x) { this.x = x; }
 	public void setY(float y) { this.y = y; }
 	
-	public Point2 additionVector(Point2 p){
+	/**
+	 * addition of two vectors, w = u + p
+	 * @param p
+	 * @return w
+	 */
+	public Point2 addition(Point2 p){
 		return new Point2(this.x + p.getX(), this.y + p.getY());
 	}
 	
-	public void addition(Point2 p){
+	/**
+	 * addition of two vectors, u = u + p
+	 * @param p
+	 */
+	public void add(Point2 p){
 		this.x += p.getX();
 		this.y += p.getY();
+	}
+	
+	/**
+	 * subtraction of two vectors, w = u - p
+	 * @param p
+	 * @return w
+	 */
+	public Point2 subtraction(Point2 p){
+		return new Point2(this.x - p.getX(), this.y - p.getY());
+	}
+	
+	/**
+	 * subtraction of two vectors, u = u - p
+	 * @param p
+	 */
+	public void substract(Point2 p){
+		this.x -= p.getX();
+		this.y -= p.getY();
+	}
+	
+	/**
+	 * product by a real, w = f * u
+	 * @param f
+	 * @return w
+	 */
+	public Point2 productPoint2(float f){
+		return new Point2(this.x * f, this.y * f);
+	}
+	
+	/**
+	 * product by a real, u = f * u
+	 * @param f
+	 */
+	public void product(float f){
+		this.x *= f;
+		this.y *= f;
+	}
+	
+	/**
+	 * division by a real, w = u / f
+	 * @param f
+	 * @return w
+	 * @throws Exception : division by zero
+	 */
+	public Point2 division(float f) throws Exception{
+		if(f == 0) throw new Exception("Division by zero");
+		float inv = 1.f / f;
+		return this.productPoint2(inv);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        
+        final Point2 p = (Point2) o;
+        return this.x == p.getX() && this.y == p.getY();
+	}
+
+	@Override
+	public String toString() {
+		return "Point2{" + this.x + ";" + this.y + "}";
 	}
 	
 }
--- test/lp/iem/gk/test/Point2Test.java
+++ test/lp/iem/gk/test/Point2Test.java
@@ -1,13 +1,20 @@
 package lp.iem.gk.test;
 
 import static org.junit.Assert.*;
+import lp.iem.gk.Point2;
 
 import org.junit.*;
 
 public class Point2Test {
-
+	
+	static final float startx = 2.f;
+	static final float starty = 3.f;
+	
+	static Point2 point;
+	
 	@BeforeClass
     public static void setUpClass() throws Exception {
+		point = new Point2(startx, starty);
     }
 
     @AfterClass
@@ -20,11 +27,53 @@ public class Point2Test {
     
     @After
     public void tearDown() {
+		point.setX(startx);
+		point.setY(starty);
     }
     
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdditionVector() {
+		Point2 p1 = new Point2(1.f, 3.f);
+		Point2 actual = point.addition(p1);
+		Point2 expected = new Point2(3.f, 6.f);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testAddition() {
+		Point2 p1 = new Point2(1.f, 3.f);
+		point.add(p1);
+		Point2 expected = new Point2(3.f, 6.f);
+		assertEquals(expected, point);
+	}
+	
+	@Test
+	public void testSubtractionVector() {
+		Point2 p1 = new Point2(1.f, 2.f);
+		Point2 actual = point.subtraction(p1);
+		Point2 expected = new Point2(1.f, 1.f);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testSubtraction() {
+		Point2 p1 = new Point2(1.f, 2.f);
+		point.substract(p1);
+		Point2 expected = new Point2(1.f, 1.f);
+		assertEquals(expected, point);
+	}
+	
+	@Test
+	public void testProductVector() {
+		Point2 actual = point.productPoint2(2.f);
+		Point2 expected = new Point2(4.f, 6.f);
+		assertEquals(expected, actual);
 	}
 
+	@Test
+	public void testProduct() {
+		point.product(2.f);
+		Point2 expected = new Point2(4.f, 6.f);
+		assertEquals(expected, point);
+	}
 }
