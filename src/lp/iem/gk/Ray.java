71497bdca3cfa70584acf66c236f44298e339553
maxence <trinquet.max@orange.fr>
2013-01-11 09:55:25 +0100

    Ray + test

---
+++ src/lp/iem/gk/Ray.java
@@ -0,0 +1,44 @@
+package lp.iem.gk;
+
+public class Ray extends BasicRay
+{
+	Vector inv_d;       //!< 1 / direction.
+	char sign_d[] = new char[4];    //!< vrai (==1) si direction[i] < 0.
+	
+	public Ray()
+	{
+		super();
+	}
+	
+	//! constructeur (origine, direction). direction est un vecteur unitaire.
+	public Ray( Point origin,  Vector direction) throws Exception
+	{
+		super(origin, direction, -1);
+		inv_d = new Vector(1.f / d.getX(), 1.f / d.getY(), 1.f / d.getZ());
+		sign_d[0] = (char) ((inv_d.get(0) < 0.f) ?  1 : 0);
+		sign_d[1] = (char) ((inv_d.get(1) < 0.f) ?  1 : 0);
+		sign_d[2] = (char) ((inv_d.get(2) < 0.f) ?  1 : 0);
+		sign_d[3] = 0;
+	}
+	
+	//! constructeur (origine, direction). direction est un vecteur unitaire.
+	public Ray( Point origin,  Vector direction, int id) throws Exception
+	{
+		super(origin, direction, id);
+		inv_d = new Vector(1.f / d.getX(), 1.f / d.getY(), 1.f / d.getZ());
+		sign_d[0] = (char) ((inv_d.get(0) < 0.f) ?  1 : 0);
+		sign_d[1] = (char) ((inv_d.get(1) < 0.f) ?  1 : 0);
+		sign_d[2] = (char) ((inv_d.get(2) < 0.f) ?  1 : 0);
+		sign_d[3] = 0;
+	}
+	
+	//! renvoie vrai si la direction du rayon est < 0 pour l'axe 'axis'.
+	public Boolean isBackward(int axis ) 
+    {
+		if (sign_d[axis] == 1)
+			return true;
+		else
+			return false;
+    }
+
+}
---
+++ test/lp/iem/gk/test/RayTest.java
@@ -0,0 +1,41 @@
+package lp.iem.test.test;
+
+
+import static org.junit.Assert.*;
+import lp.iem.gk.Point;
+import lp.iem.gk.Ray;
+import lp.iem.gk.Vector;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RayTest
+{
+	static Point p;
+	static Vector v;
+	static Ray ray;
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception
+	{
+		p = new Point(1,0,0);
+		v = new Vector(1,1,1);
+		
+		ray = new Ray(p,v,5);
+	}
+	
+	@Test
+	public void testConstruct()
+	{
+		assertNotNull(ray);
+	}
+	
+	@Test
+	public void testBacward()
+	{
+		Boolean bol = ray.isBackward(0);
+		assertTrue(bol == false);
+		assertTrue(bol != true);
+	}
+
+}
